bdbc61475a21e3f51f0b5b575e06098f725f5174
Theo Kapi <theo_kapi@outlook.com>
2026-05-26 22:05:35 -0700

    Adding direction to jet engine

--- src/main/java/com/unlimitedstuffltd/AirplanesDataGenerator.java
+++ src/main/java/com/unlimitedstuffltd/AirplanesDataGenerator.java
@@ -1,6 +1,5 @@
 package com.unlimitedstuffltd;
 
-import com.unlimitedstuffltd.datagen.AirplanesModelProvider;
 import com.unlimitedstuffltd.datagen.AirplanesRecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
@@ -11,6 +10,5 @@ public class AirplanesDataGenerator implements DataGeneratorEntrypoint {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
 		pack.addProvider(AirplanesRecipeProvider::new);
-		pack.addProvider(AirplanesModelProvider::new);
 	}
 }
--- src/main/java/com/unlimitedstuffltd/JetEngineBlock.java
+++ src/main/java/com/unlimitedstuffltd/JetEngineBlock.java
@@ -1,9 +1,59 @@
 package com.unlimitedstuffltd;
 
+import net.minecraft.core.Direction;
+import net.minecraft.core.Direction.Axis;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import org.jspecify.annotations.NonNull;
 
 public class JetEngineBlock extends Block {
-    public JetEngineBlock(Properties properties) {
+    public static final EnumProperty<Direction.Axis> AXIS;
+
+    public JetEngineBlock(BlockBehaviour.Properties properties) {
         super(properties);
+
+        registerDefaultState(defaultBlockState().setValue(AXIS, Axis.Y));
+    }
+
+    protected @NonNull BlockState rotate(@NonNull BlockState state, @NonNull Rotation rotation) {
+        return rotatePillar(state, rotation);
+    }
+
+    public static BlockState rotatePillar(BlockState state, Rotation rotation) {
+        switch (rotation) {
+            case COUNTERCLOCKWISE_90:
+            case CLOCKWISE_90:
+                switch (state.getValue(AXIS)) {
+                    case X -> {
+                        return state.setValue(AXIS, Axis.Z);
+                    }
+                    case Z -> {
+                        return state.setValue(AXIS, Axis.X);
+                    }
+                    default -> {
+                        return state;
+                    }
+                }
+            default:
+                return state;
+        }
+    }
+
+    protected void createBlockStateDefinition(final StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(AXIS);
+    }
+
+    public BlockState getStateForPlacement(final BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(AXIS, context.getClickedFace().getAxis());
+    }
+
+    static {
+        AXIS = BlockStateProperties.AXIS;
     }
 }
